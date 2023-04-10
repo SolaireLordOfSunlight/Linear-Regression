4a5f64930e13e44332e41bac5c2c918834e81ebe
SolaireLordOfSunlight <rudy.miranda.bass@gmail.com>
2023-04-09 23:06:15 -0400

    first try

---
+++ tarea.R
@@ -0,0 +1,45 @@
+table_values <- read.csv("./nba_stats.csv")
+
+plot(table_values$MPG, table_values$PPG)
+
+l_model <- summary(lm(formula = PPG ~ MPG, data = table_values))
+
+fx <- function(x) l_model$coefficients[1] + l_model$coefficients[2] * x
+
+eps <- table_values$PPG - fx(table_values$MPG)
+
+summary(eps)
+
+library(tseries)
+library(nortest)
+jarque.bera.test(eps)
+shapiro.test(eps)
+ad.test(eps)
+ks.test(eps, "pnorm")
+
+t.test(eps, mu = 0)
+
+qqnorm(eps)
+
+### Graph
+
+library(ggplot2)
+
+ggplot(table_values, aes(MPG, PPG)) +
+    geom_point() +
+    geom_smooth(method = "lm", se = FALSE) +
+    ggtitle("PPG = -1.92 + 0.53MPG")
+
+ggplot(as.data.frame(eps), aes(sample = eps)) +
+    stat_qq(distribution = stats::qnorm) +
+    stat_qq_line(distribution = stats::qnorm)
+
+library(ggpubr)
+
+theme_set(theme_minimal())
+
+png("plot2.png")
+ggqqplot(as.data.frame(eps), x = "eps",
+    ggtheme = theme_minimal(),
+    title = "QQPlot")
+dev.off()
